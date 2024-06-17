1c540eee62343aca3d825fc2bdfe7b2a3d946b21
ChethanaKavi <138358211+ChethanaKavi@users.noreply.github.com>
2024-06-17 21:28:57 +0530

    Update volumeOfSphere.scala

--- src/main/scala/volumeOfSphere.scala
+++ src/main/scala/volumeOfSphere.scala
@@ -1,5 +1,5 @@
 object volumeOfSphere {
-  def volumeOfSphere(r: Double): Double = 4/3*math.Pi * r * r*r
+  def volumeOfSphere(r: Double): Double = 4*math.Pi *r*r*r/3
 
   def main(args: Array[String]): Unit = {
     val r = 5
